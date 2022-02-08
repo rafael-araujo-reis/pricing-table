cca1d28d71657a99f93ab6e15168e9062038bc3a
Rafael Reis <rafa.araujoreis@gmail.com>
2022-02-08 10:55:05 -0300

    preparando a estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Conheça nossos planos</h1>
+        <p>Conheça nossas soluções e escolha o plano ideal para o seu próximo projeto</p>
       </header>
-    </div>
+
+      <div className="planos">
+        <div className="card">
+          <div className="header">
+            <p>Básico</p>
+            <span>Popular</span>
+          </div>
+          <div className="price">
+            <h2>
+              R$ 29<span>,00</span>
+              <span>/mês</span>
+            </h2>
+          </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate consequatur eos quod non sunt deleniti cumque aliquam enim voluptate totam.</p>
+
+          <ul>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+          </ul>
+
+          <button>Assinar</button>
+        </div>
+
+        <div className="card">
+          <div className="header">
+            <p>Básico</p>
+            <span>Popular</span>
+          </div>
+          <div className="price">
+            <h2>
+              R$ 29<span>,00</span>
+              <span>/mês</span>
+            </h2>
+          </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate consequatur eos quod non sunt deleniti cumque aliquam enim voluptate totam.</p>
+
+          <ul>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+          </ul>
+
+          <button>Assinar</button>
+        </div>
+
+        <div className="card">
+          <div className="header">
+            <p>Básico</p>
+            <span>Popular</span>
+          </div>
+          <div className="price">
+            <h2>
+              R$ 29<span>,00</span>
+              <span>/mês</span>
+            </h2>
+          </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate consequatur eos quod non sunt deleniti cumque aliquam enim voluptate totam.</p>
+
+          <ul>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+          </ul>
+
+          <button>Assinar</button>
+        </div>
+      </div>
+    </>
   );
 }
 
